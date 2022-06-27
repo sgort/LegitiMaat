1d5135bb8e00e10dd0d9007370a908be821dde33
datafluisteraar <steven.gort@ictu.nl>
2022-06-27 09:10:36 +0200

    reversed resizing logo

--- js/config.js
+++ js/config.js
@@ -81,8 +81,8 @@ var respecConfig =
     src: "https://open-regels.nl/assets/images/legitimaat-logo.svg",
     alt: "LegitiMaat-logo",
     id: "MinBZK",
-    height: 600,
-    width: 600,
+    height: 300,
+    width: 300,
     url: "https://open-regels.nl",
   },
 
